eda900c6ef1c26d2be595d5b5ad03243b1fb54ee
niltoneapontes <niltoneapontes@gmail.com>
2020-11-05 09:59:35 -0300

    Fixing logo

--- components/component/intro.tsx
+++ components/component/intro.tsx
@@ -6,31 +6,31 @@ import Image from 'next/image';
 
 const Intro = () => {
   return (
-      <ToggleContainer>
-        <input id="toggle" type="checkbox"/>
-        <label htmlFor="toggle"></label>
-        <IntroContainer>
-          <AlignLogo>
-            <Image src='/../public/assets/logo.png' width={52} height={72} alt="Logo" />
-            <h1>
-                Little Coffee
+    <ToggleContainer>
+      <input id="toggle" type="checkbox" />
+      <label htmlFor="toggle"></label>
+      <IntroContainer>
+        <AlignLogo>
+          <Image src='https://imgur.com/rgZI9dv.png' width={52} height={72} alt="Logo" />
+          <h1>
+            Little Coffee
             </h1>
-          </AlignLogo>
-          
-          <ButtonContainer>
-            <Link href="/"><Button>Home</Button></Link>
-            <Link href="/authors"><Button>Authors</Button></Link>
-            <Link href="/contact"><Button>Contact</Button></Link>
-            <Link href="https://open.spotify.com/playlist/1e1c2jKIvccmz9aVU6Kk8j"><Button>Playlist</Button></Link>
-          </ButtonContainer>
+        </AlignLogo>
 
-          <SocialMedia>
-            <a href="#"><FaGithub size={32}/></a>
-            <a href="#"><FaTwitter size={32}/></a>
-            <a href="#"><FaLinkedinIn size={32}/></a>
-          </SocialMedia>
-        </IntroContainer>
-      </ToggleContainer>
+        <ButtonContainer>
+          <Link href="/"><Button>Home</Button></Link>
+          <Link href="/authors"><Button>Authors</Button></Link>
+          <Link href="/contact"><Button>Contact</Button></Link>
+          <Link href="https://open.spotify.com/playlist/1e1c2jKIvccmz9aVU6Kk8j"><Button>Playlist</Button></Link>
+        </ButtonContainer>
+
+        <SocialMedia>
+          <a href="#"><FaGithub size={32} /></a>
+          <a href="#"><FaTwitter size={32} /></a>
+          <a href="#"><FaLinkedinIn size={32} /></a>
+        </SocialMedia>
+      </IntroContainer>
+    </ToggleContainer>
   )
 }
 
