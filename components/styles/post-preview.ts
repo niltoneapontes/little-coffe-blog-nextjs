f5d1f2e551ecefdd9a99953d7fdcc82c7da7c342
Nilton Pontes <nilton.pontes@jbq.global>
2021-02-11 16:32:05 -0300

    style: break word on post preview"

--- components/styles/post-preview.ts
+++ components/styles/post-preview.ts
@@ -15,6 +15,8 @@ export const PostPreviewContainer = styled.section`
     font-family: 'Montserrat', sans-serif;
     font-size: 32px;
     font-weight: 800;
+    word-break: break-all;
+    word-wrap: break-word;
 
     a {
       font-family: 'Montserrat', sans-serif;
